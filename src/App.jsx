f15d9da9c8f372da5df84f564b2d8389c9fc25d8
Bhaumik Chauhan <127184237+bhaumik0307@users.noreply.github.com>
2024-06-11 23:01:37 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,18 @@ import {
 } from "react-router-dom";
 
 const App = () => {
+  const apikey = import.meta.env.VITE_NEWS_API_KEY
   const pageSize = 6
     const router = createBrowserRouter(
       createRoutesFromElements(
         <Route exact path="/" element={<Layout />}>
-          <Route exact path = "" element = {<News key="general" pageSize = {pageSize} category = 'general' />} />
-          <Route exact path = "business" element = {<News key="business" pageSize = {pageSize} category = 'business' />} />
-          <Route exact path = "entertainment" element = {<News key="entertainment" pageSize = {pageSize} category = 'entertainment' />} />
-          <Route exact path = "health" element = {<News key="health"  pageSize = {pageSize} category = 'health' />} />
-          <Route exact path = "science" element = {<News key="science" pageSize = {pageSize} category = 'science' />} />
-          <Route exact path = "sports" element = {<News key="sports"  pageSize = {pageSize} category = 'sports' />} />
-          <Route exact path = "technology" element = {<News key="technology" pageSize = {pageSize} category = 'technology' />} />
+          <Route exact path = "" element = {<News key="general" pageSize = {pageSize} apikey={apikey} category = 'general' />} />
+          <Route exact path = "business" element = {<News key="business" pageSize = {pageSize} apikey={apikey} category = 'business' />} />
+          <Route exact path = "entertainment" element = {<News key="entertainment" pageSize = {pageSize} apikey={apikey} category = 'entertainment' />} />
+          <Route exact path = "health" element = {<News key="health"  pageSize = {pageSize} apikey={apikey} category = 'health' />} />
+          <Route exact path = "science" element = {<News key="science" pageSize = {pageSize} apikey={apikey} category = 'science' />} />
+          <Route exact path = "sports" element = {<News key="sports"  pageSize = {pageSize} apikey={apikey} category = 'sports' />} />
+          <Route exact path = "technology" element = {<News key="technology" pageSize = {pageSize} apikey={apikey} category = 'technology' />} />
         </Route>
       )
     )
